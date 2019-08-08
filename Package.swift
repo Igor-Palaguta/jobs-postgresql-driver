3f193a930653c97e34c1ac3b6d5ae8b058c06e1a
Igor Palaguta <igor.palaguta@zalando.de>
2019-08-08 09:41:00 +0300

    One Thread

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
   dependencies: [
     .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-    .package(url: "https://github.com/vapor-community/jobs.git", from: "0.2.0"),
+    .package(url: "https://github.com/Igor-Palaguta/jobs.git", .branch("OneThread")),
     .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0")
   ],
   targets: [
